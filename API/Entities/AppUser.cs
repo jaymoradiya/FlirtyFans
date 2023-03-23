261895592ae130191fb372d356e5f0bd08ebc399
Jay Moradiya <jaymoradiya0@gmail.com>
2023-03-23 17:37:57 +0530

    init

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+
+        public int Id { get; set; }
+        public String UserName { get; set; }
+    }
+}
---
+++ API/Program.cs
@@ -0,0 +1,22 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+app.MapControllers();
+
+app.Run();
