cc713158885dbf8faab618fe0cc01973919c91ad
Jay Moradiya <jaymoradiya0@gmail.com>
2023-03-24 10:17:08 +0530

    Angular init

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,10 +1,7 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace API.Entities
 {
     public class AppUser
     {
-
         public int Id { get; set; }
         public String UserName { get; set; }
     }
--- API/Program.cs
+++ API/Program.cs
@@ -14,8 +14,12 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
+
 
 var app = builder.Build();
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
